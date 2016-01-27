c716c0ca0f8925c543237fe816a412e87acf3c48
Nathan <nathan.lea@okstate.edu>
2016-01-26 22:42:46 -0600

    Initial Commit of Project

---
+++ Secure Camera Capture Client/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Secure_Camera_Capture_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.flowLayoutPanel1.Controls.Add(this.treeView1);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 27);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1066, 470);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 27);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 23);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.settingsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(74, 23);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView1.Font = new System.Drawing.Font("Gill Sans MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(263, 454);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(272, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(783, 454);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1067, 496);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(1083, 535);
+            this.MinimumSize = new System.Drawing.Size(1083, 535);
+            this.Name = "Form1";
+            this.Text = "Secure Camera Capture Client";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Secure Camera Capture Client/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Secure_Camera_Capture_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            treeView1.DrawMode = TreeViewDrawMode.OwnerDrawText;
+            treeView1.DrawNode += new DrawTreeNodeEventHandler(treeView1_DrawNode);
+        }
+
+        void treeView1_DrawNode(object sender, DrawTreeNodeEventArgs e)
+        {
+
+            if ((e.State & TreeNodeStates.Selected) == TreeNodeStates.Selected)
+            {
+                Graphics g = e.Graphics;
+                // get node font and node fore color
+                Font nodeFont = GetTreeNodeFont(e.Node);
+                Color nodeForeColor = GetTreeNodeForeColor(e.Node, e.State);
+
+                // fill node background
+                using (SolidBrush brush = new SolidBrush(Color.White))
+                {
+                    e.Graphics.FillRectangle(brush, e.Bounds);
+                }
+
+                // draw node text
+                TextRenderer.DrawText(e.Graphics, e.Node.Text, nodeFont, e.Bounds, nodeForeColor, TextFormatFlags.Left | TextFormatFlags.Top);
+
+                using (Pen pen = new Pen(nodeForeColor))
+                {
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                    Rectangle penBounds = e.Bounds;
+                    penBounds.Width -= 1;
+                    penBounds.Height -= 1;
+                    e.Graphics.DrawRectangle(pen, penBounds);
+                }
+            }
+            else
+            {
+                e.DrawDefault = true;
+            }
+        }
+
+        private Font GetTreeNodeFont(TreeNode node)
+        {
+            Font nodeFont = node.NodeFont;
+            if (nodeFont == null)
+            {
+                nodeFont = this.Font;
+            }
+            return nodeFont;
+        }
+
+        private Color GetTreeNodeForeColor(TreeNode node, TreeNodeStates nodeState)
+        {
+            Color nodeForeColor = Color.Empty;
+
+            if ((nodeState & TreeNodeStates.Selected) == TreeNodeStates.Selected)
+            {
+                nodeForeColor = Color.FromKnownColor(KnownColor.HighlightText);
+            }
+            else
+            {
+                nodeForeColor = node.ForeColor;
+                if (nodeForeColor == Color.Empty)
+                {
+                    nodeForeColor = this.ForeColor;
+                }
+            }
+
+            return nodeForeColor;
+        }
+
+private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            //
+            // This is the first node in the view.
+            //
+            TreeNode treeNode = new TreeNode("2013");
+            treeView1.Nodes.Add(treeNode);
+            //
+            // Another node following the first node.
+            //
+            treeNode = new TreeNode("2014");
+            treeView1.Nodes.Add(treeNode);
+            //
+            // Create two child nodes and put them in an array.
+            // ... Add the third node, and specify these as its children.
+            //
+            TreeNode node2 = new TreeNode("January");
+            TreeNode node3 = new TreeNode("Febuary");
+            TreeNode[] array = new TreeNode[] { node2, node3 };
+            //
+            // Final node.
+            //
+            treeNode = new TreeNode("2015", array);
+            treeView1.Nodes.Add(treeNode);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeView tv = (TreeView)sender;
+            //tv.SelectedNode.BackColor = Color.DarkOrange;
+            tv.HideSelection = false;
+        }
+    }
+}
