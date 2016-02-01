e9dbc9477349ed5b42680c5c1a14972cc165501c
nathanlea <nathan.lea@okstate.edu>
2016-02-01 16:40:24 -0600

    JSONParser about half done, JsonObject done, added StringExtension.cs to GetLast() on string for minute parse

--- Secure Camera Capture Client/JSONParser.cs
+++ Secure Camera Capture Client/JSONParser.cs
@@ -12,6 +12,7 @@ namespace Secure_Camera_Capture_Client
         public JSONParser (String JSONfile)
         {
             string testString = System.IO.File.ReadAllText(@"C:\Users\Nathan\Desktop\testing.json");
+            string jsonString = Regex.Replace(testString, @"\s+", "").ToString();
             //New JsonObject
             JsonObject jO = new JsonObject();
             //Get id
@@ -20,12 +21,191 @@ namespace Secure_Camera_Capture_Client
             jO.date_created = Regex.Match(testString, "\"date_created\"[ :]+(\"[^\"]*\")").ToString();
             var stringLeftToIndex = testString.Length;
             var currentPosInString = 0;
+            //Pull off the first characters
+            currentPosInString += jO.id.Length + jO.date_created.Length + 2; //The extra 3 is the :'s and the {
+            stringLeftToIndex -= currentPosInString;
+            
+            //Set list counters for levels of listing
+            int yearLevel  = -1;
+            int monthLevel = -1;
+            int dayLevel   = -1;
+            int hourLevel  = -1;
+
+            //Starting at the year level
             while ( stringLeftToIndex > 0 )
             {
-                
+                //Get the year
+                var year = jsonString.Substring(currentPosInString, 4);
+                //Update counters
+                currentPosInString += 4; stringLeftToIndex -= 4;
+
+                //Create Object
+                jO.year.Add(new JsonObject.Year(Convert.ToInt32(year)));
+                yearLevel++;
+
+                while (stringLeftToIndex > 0)
+                {
+                    //Get the month
+                    currentPosInString += 3; stringLeftToIndex -= 3;
+                    var month = "";
+                    var monthBlock = jsonString.Substring(currentPosInString, 10);
+                    month = Regex.Match(monthBlock, "\"([^\\)]+)\"").ToString();
+                    month = Regex.Replace(month, "\"", "").ToString();
+                    //Update counters
+                    currentPosInString += month.Length + 2; stringLeftToIndex -= month.Length + 2;
+
+                    //Create Object
+                    jO.year.ElementAt(yearLevel).months.Add(new JsonObject.Month(getMonthInt(month)));
+                    monthLevel++;
+
+                    while (stringLeftToIndex > 0)
+                    {
+                        //Get the day
+                        currentPosInString += 2; stringLeftToIndex -= 2;
+                        var day = "";
+                        var dayBlock = jsonString.Substring(currentPosInString, 4);
+                        day = Regex.Match(dayBlock, "\"([^\\)]+)\"").ToString();
+                        day = Regex.Replace(day, "\"", "").ToString();
+                        //Update counters
+                        currentPosInString += day.Length + 2; stringLeftToIndex -= day.Length + 2;
+
+                        //Create Object
+                        jO.year.ElementAt(yearLevel).months.ElementAt(monthLevel).days.Add(new JsonObject.Day(Convert.ToInt16(day)));
+                        dayLevel++;
+
+                        while ( stringLeftToIndex > 0)
+                        {
+                            //Get the hour
+                            currentPosInString += 2; stringLeftToIndex -= 2;
+                            var hour = "";
+                            var hourBlock = jsonString.Substring(currentPosInString, 4);
+                            hour = Regex.Match(hourBlock, "\"([^\\)]+)\"").ToString();
+                            hour = Regex.Replace(hour, "\"", "").ToString();
+                            //Update counters
+                            currentPosInString += hour.Length + 3; stringLeftToIndex -= hour.Length + 3;
+
+                            //Create object
+                            jO.year.ElementAt(yearLevel).months.ElementAt(monthLevel).days.ElementAt(dayLevel).hours.Add(new JsonObject.Hour(Convert.ToInt16(hour)));
+                            hourLevel++;
+
+                            int hourBlockEnd = 0;
+                            int i = currentPosInString;
+                            while(stringLeftToIndex > 0)
+                            {
+                                if(jsonString.ElementAt(i) == ']')
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    i++;
+                                    hourBlockEnd++;                                   
+                                }
+                            }
+
+                            hourBlock = jsonString.Substring(currentPosInString, hourBlockEnd);
+
+                            //Get the objects in the hour block
+                            var imageMatches = Regex.Match(hourBlock, "\\{([^\\}]+)\\}");
+
+                            //Get first match
+                            string minute = "";
+                            string date_taken = "";
+                            string file_name = "";
+                            string method = "";
+
+                            string firstMatch = imageMatches.ToString();
+                            file_name = Regex.Match(firstMatch, "\"file_name\"[ :]+(\"[^\"]*\")").ToString().Substring(13);
+                            date_taken = Regex.Match(firstMatch, "\"date_taken\"[ :]+(\"[^\"]*\")").ToString().Substring(14);
+                            method = Regex.Match(firstMatch, "\"method\"[ :]+(\"[^\"]*\")").ToString().Substring(10);
+
+                            file_name = Regex.Replace(file_name, "\"", "").ToString();
+                            date_taken = Regex.Replace(date_taken, "\"", "").ToString();
+                            method = Regex.Replace(method, "\"", "").ToString();
+
+                            minute = date_taken.GetLast(2);
+
+                            Console.WriteLine(file_name);
+                            Console.WriteLine(date_taken);
+                            Console.WriteLine(method);
+                            Console.WriteLine(minute);
+
+
+                            jO.year.ElementAt(yearLevel).months.ElementAt(monthLevel).days.ElementAt(dayLevel).hours.ElementAt(hourLevel).images.Add(new JsonObject.Image(Convert.ToInt16(minute), file_name, date_taken, getMethodInt(method)));
+                            //Get more matches
+                            string match = imageMatches.NextMatch().ToString();
+                            string oldmatch = firstMatch;
+                            while ( match != oldmatch )
+                            {
+                                file_name = Regex.Match(match, "\"file_name\"[ :]+(\"[^\"]*\")").ToString().Substring(13);
+                                date_taken = Regex.Match(match, "\"date_taken\"[ :]+(\"[^\"]*\")").ToString().Substring(14);
+                                method = Regex.Match(match, "\"method\"[ :]+(\"[^\"]*\")").ToString().Substring(10);
+
+                                file_name = Regex.Replace(file_name, "\"", "").ToString();
+                                date_taken = Regex.Replace(date_taken, "\"", "").ToString();
+                                method = Regex.Replace(method, "\"", "").ToString();
+
+                                minute = date_taken.GetLast(2);
+
+                                jO.year.ElementAt(yearLevel).months.ElementAt(monthLevel).days.ElementAt(dayLevel).hours.ElementAt(hourLevel).images.Add(new JsonObject.Image(Convert.ToInt16(minute), file_name, date_taken, getMethodInt(method)));
+
+                                //Update match
+                                oldmatch = match;
+                                match = imageMatches.NextMatch().ToString();
+                            }
+                            break;
+                        }
+
+                    }
+                }
             }            
 
         }
 
+        public Int16 getMonthInt(string month)
+        {
+            switch ( month ) {
+                case "January":
+                    return 1;
+                case "February":
+                    return 2;
+                case "March":
+                    return 3;
+                case "April":
+                    return 4;
+                case "May":
+                    return 5;
+                case "June":
+                    return 6;
+                case "July":
+                    return 7;
+                case "August":
+                    return 8;
+                case "September":
+                    return 9;
+                case "October":
+                    return 10;
+                case "November":
+                    return 11;
+                case "December":
+                    return 12;
+                default:
+                    return -1;
+            }
+
+        }
+
+        public Int16 getMethodInt(string method)
+        {
+            switch (method)
+            {
+                case "timer":
+                    return 1;
+                case "motion":
+                    return 2;
+                default:
+                    return -1;
+            }
+        }
     }
 }
--- Secure Camera Capture Client/JsonObject.cs
+++ Secure Camera Capture Client/JsonObject.cs
@@ -10,16 +10,36 @@ namespace Secure_Camera_Capture_Client
     {
         public string id { get; set; }
         public string date_created { get; set; }
+        public string method { get; set; }
+        public List<Year> year { get; set; } 
+
+        public JsonObject ()
+        {
+            year = new List<Year>();
+        }
 
         public class Image
         {
+            public Image(Int16 minute, string file_name, string date_taken, Int16 method)
+            {
+                this.minute = minute;
+                this.file_name = file_name;
+                this.date_taken = date_taken;
+                this.method = method;
+            }
             public Int16 minute { get; set; }
             public string file_name { get; set; }
             public string date_taken { get; set; }
+            public Int16 method { get; set; }
         }
 
         public class Hour
         {
+            public Hour(Int16 hour)
+            {
+                this.hour = hour;
+                this.images = new List<Image>();
+            }
             //This is the hour in the day, 0-23
             public Int16 hour { get; set; }
             public IList<Image> images { get; set; }
@@ -27,6 +47,11 @@ namespace Secure_Camera_Capture_Client
 
         public class Day
         {
+            public Day(Int16 day_name)
+            {
+                this.day_name = day_name;
+                this.hours = new List<Hour>();
+            }
             //This is the number of the day in the month 1-31ish
             public Int16 day_name { get; set; }
             public IList<Hour> hours { get; set; }
@@ -34,6 +59,11 @@ namespace Secure_Camera_Capture_Client
 
         public class Month
         {
+            public Month(Int16 month_name)
+            {
+                this.month_name = month_name;
+                this.days = new List<Day>();
+            }
             //This is 1-12 January-December Respective
             public Int16 month_name { get; set; }
             public IList<Day> days { get; set; }
@@ -42,6 +72,12 @@ namespace Secure_Camera_Capture_Client
 
         public class Year
         {
+            public Year(Int32 year_name)
+            {
+                this.year_name = year_name;
+                this.months = new List<Month>();
+            }
+            
             //Int value of the year such as 2016
             public Int32 year_name { get; set; }
             public IList<Month> months { get; set; }
